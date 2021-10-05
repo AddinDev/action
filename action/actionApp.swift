5e935dee0afc5e402329c688052519ff10094f8a
AddinDev <addinsatria2004@gmail.com>
2021-10-05 13:27:32 +0700

    Initial Commit

---
+++ action/actionApp.swift
@@ -0,0 +1,17 @@
+//
+//  actionApp.swift
+//  action
+//
+//  Created by Addin Satria on 05/10/21.
+//
+
+import SwiftUI
+
+@main
+struct actionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
